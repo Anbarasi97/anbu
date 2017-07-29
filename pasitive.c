b01aefce98e7137f0e275f8a45b9ea215dff3fc0
Anbarasi97 <30333678+Anbarasi97@users.noreply.github.com>
2017-07-29 16:51:48 +0530

    Create pasitive.c

---
+++ pasitive.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n;
+/* enter the number */
+scanf("%d",&n);
+if(n==0)
+{
+ printf("zero");
+}
+else if(n>0)
+{
+printf("positive");
+}
+else
+{
+printf("nagative");
+}
+
+return 0;
+}
